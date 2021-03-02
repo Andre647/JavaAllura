e02d607070bbae1a621ad45590b30d02821dd9c8
Deca <41493871+Pitucho1@users.noreply.github.com>
2021-03-02 18:09:08 -0300

    Socorro

--- src/monteiro/andre/Main.java
+++ src/monteiro/andre/Main.java
@@ -3,6 +3,6 @@ package monteiro.andre;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Ola Mundo");
+        System.out.println("Mano to perdido");
     }
 }
